25be13a802cdc9530518233f7c07b3c7e6f12b49
edie <paulolopez92@outlook.com>
2020-08-22 08:46:08 -0300

    fix some bugs and ajust some things

--- ipscan.sh
+++ ipscan.sh
@@ -8,51 +8,69 @@ fi
 echo "##################################################################################"
 echo "                                welcome to my ipscan                             "
 echo "##################################################################################"
-
+echo ""
 
 if [ "$1" == "-h" ] ; then
-	echo "Usage mode: ./ipscan [ARGUMENTS...]  <ip array to scan>"
+	echo "Usage mode: ./ipscan [ARGUMENTS...]  [Ip Range to Scan]"
 	echo "-i to simple scan"
 	echo "-io to gen file output"
 	echo "-ic to clean scan"
 	echo "-ioc to scan with clean output file"
 	echo "Example:"
-	echo "./ipscan -i 10.10.10 =====>> For simple scan."
-	echo "./ipscan -io 10.10.10 =====>> For scan with output file."
-	echo "./ipscan -ic 10.10.10  =====>> For clean scan [to clean output]."
-	echo "./ipscan -ioc 10.10.10  =====>> For clean output scan [to clean output file type wordlist] "
+	echo "./ipscan -i 10.10.10.1/24 =====>> For simple scan."
+	echo "./ipscan -io 10.10.10.1/24 =====>> For scan with output file."
+	echo "./ipscan -ic 10.10.10.1/24  =====>> For clean scan [to clean output]."
+	echo "./ipscan -ico 10.10.10.1/24  =====>> For clean output scan [to clean output file like wordlist] "
 fi
-if [ "$1" == "-id" ] ; then
+
+if [ "$1" == "-i" ] ; then
 	for se in $(seq 1 254)
 	do
+		ip=$(echo $2 | sed 's/....$//')
 		echo "==============<<V>>============="
-		ping  $2.$se -c 2 | grep "icmp_seq=1 ttl" | cut -d " " -f 4 | sed 's/.$/ <<==Host is Up!/'
+		ping  $ip$se -c 2 | grep "icmp_seq=1 ttl" | cut -d " " -f 4 | sed 's/.$/ <<==Host is Up!/'
 	done
+echo ""
 echo "################################ ping scan ended #################################"
 fi
 
-if [ "$1" == "-ic" ] ; then   
+if [ "$1" == "-ic" ] ; then
         for se in $(seq 1 254)
         do
-		ping  $2.$se -c 2 | grep "icmp_seq=1 ttl" | cut -d " " -f 4 | sed 's/.$/ /'
+                ip=$(echo $2 | sed 's/....$//')
+		ping  $ip$se -c 2 | grep "icmp_seq=1 ttl" | cut -d " " -f 4 | sed 's/.$/ /'
         done
+echo ""
 echo "################################ ping scan ended #################################"
 fi
 
-if [ "$1" == "-ioc" ] ; then
+if [ "$1" == "-io" ] ; then
+        touch scanout.txt
+        for se in $(seq 1 254)
+        do
+                ip=$(echo $2 | sed 's/....$//')
+		echo "===============<<V>>=============="
+                ping  $ip$se -c 2 >> scanout.txt
+                echo "Testing "$ip$se" | "$se" of 254"
+        done
+echo ""
+echo "################################ The hots Up is ###################################"
+        grep "icmp_seq=1 ttl" $(pwd)/scanout.txt | cut -d " " -f 4 | sed 's/.$/ <<==Host is Up!/'
+echo "################################ ping scan ended ##################################"
+fi
+
+if [ "$1" == "-ico" ] ; then
 	touch scanout.txt
 	touch scanout-clean.txt
-	for se in $(seq 1 10)
+	for se in $(seq 1 254)
 	do
-		ping  $2.$se -c 2 >> scanout.txt
-		echo "Testing "$2"."$se" | "$se" of 254"
+                ip=$(echo $2 | sed 's/....$//')
+		ping  $ip$se -c 2 >> scanout.txt
+		echo "Testing "$ip$se" | "$se" of 254"
 	done
+echo ""
 echo "################################ The hots Up is ###################################"
 	grep "icmp_seq=1 ttl" $(pwd)/scanout.txt | cut -d " " -f 4 | sed 's/.$/ /'
 	grep "icmp_seq=1 ttl" $(pwd)/scanout.txt | cut -d " " -f 4 | sed 's/.$/ /' >> scanout-clean.txt
 echo "################################ ping scan ended ##################################"
 fi
-
-
-
-
